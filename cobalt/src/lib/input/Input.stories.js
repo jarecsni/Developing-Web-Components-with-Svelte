153a3795988800cafda565b8be8dd4ade2fda091
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-03-17 18:39:05 +0000

    Working basic Storybook test - yaaaaay

---
+++ cobalt/src/lib/input/Input.stories.js
@@ -0,0 +1,15 @@
+import Input from './Input.svelte';
+
+export default {
+  title: 'Cobalt UI Library / Basic Components / Input',
+  component: Input,
+};
+
+const Template = props => ({
+  Component: Input,
+  props
+});
+
+export const Default = Template.bind({});
+Default.args = {};
+  
