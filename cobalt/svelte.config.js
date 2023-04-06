684ce39f90a4bd8e62c1740e6bf4a7bf97cdcbb9
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-04-06 07:29:51 +0200

    Fix issue around build config for web component

--- cobalt/svelte.config.js
+++ cobalt/svelte.config.js
@@ -1,7 +1,28 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
+function isWebComponentSvelte(code) {
+  const svelteOptionsIdx = code.indexOf('<svelte:options ')
+  if(svelteOptionsIdx < 0) {
+      return false
+  }
+  const tagOptionIdx = code.indexOf('tag=', svelteOptionsIdx)
+  const svelteOptionsEndIdx = code.indexOf('>',svelteOptionsIdx);
+  return tagOptionIdx > svelteOptionsIdx && tagOptionIdx < svelteOptionsEndIdx
+}
+
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  vitePlugin: {
+    experimental: {
+        dynamicCompileOptions({code}) {
+            if(isWebComponentSvelte(code)) {
+                return {
+                    customElement: true
+                }
+            }
+        }
+    }
+  }
 }
