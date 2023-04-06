4b7d34fbf0c4f092f26094d30f3bfa83f4d60dff
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-04-06 07:49:46 +0200

    Using Svelte CSF for stories

---
+++ cobalt/src/lib/input/Input.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+    import Input from './Input.svelte';
+</script>
+
+<Meta title="Button" component={Input}/>
+
+<Template let:args>
+  <Input {...args}/>
+</Template>
+
+<Story name="Default" args={{}}/>
+  
