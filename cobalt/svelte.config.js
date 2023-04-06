8394a59378765818c6fbfac9a8315a5c95f3d524
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-04-06 07:38:00 +0200

    Removed config change - it didnt work

--- cobalt/svelte.config.js
+++ cobalt/svelte.config.js
@@ -1,28 +1,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-function isWebComponentSvelte(code) {
-  const svelteOptionsIdx = code.indexOf('<svelte:options ')
-  if(svelteOptionsIdx < 0) {
-      return false
-  }
-  const tagOptionIdx = code.indexOf('tag=', svelteOptionsIdx)
-  const svelteOptionsEndIdx = code.indexOf('>',svelteOptionsIdx);
-  return tagOptionIdx > svelteOptionsIdx && tagOptionIdx < svelteOptionsEndIdx
-}
-
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  vitePlugin: {
-    experimental: {
-        dynamicCompileOptions({code}) {
-            if(isWebComponentSvelte(code)) {
-                return {
-                    customElement: true
-                }
-            }
-        }
-    }
-  }
+  preprocess: vitePreprocess()
 }
