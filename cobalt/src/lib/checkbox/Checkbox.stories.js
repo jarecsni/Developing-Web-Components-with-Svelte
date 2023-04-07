3d7adc55705eff420db4c67a1aa9d0f2a56eedd8
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-04-07 11:57:36 +0200

    Added checkbox story

---
+++ cobalt/src/lib/checkbox/Checkbox.stories.js
@@ -0,0 +1,16 @@
+import Checkbox from './Checkbox.svelte';
+
+
+export default {
+  title: 'Cobalt UI Library / Basic Components / Checkbox',
+  component: Checkbox
+};
+
+const Template = props => ({
+  Component: Checkbox,
+  props
+});
+
+export const Default = Template.bind({});
+Default.args = {};
+  
---
+++ cobalt/src/lib/checkbox/Checkbox.svelte
@@ -0,0 +1,26 @@
+<svelte:options tag="cobalt-checkbox"/>
+
+<div class="cobalt">
+    <input 
+        type="checkbox"
+        id="name"
+        {checked}
+        {disabled}
+        on:change={onChange}
+        {...$$props}
+    />
+    <label for="name">
+        {label}
+    </label>
+</div>
+
+<script>
+    import './checkbox.css';
+    export let checked = true;
+    export let label = "Checkbox";
+    export let disabled = false;
+    function onChange(event) {
+        event.target.dispatchEvent(new CustomEvent("onchange", { composed: true }));
+    }
+    $: checked = checked !== false;
+</script>
